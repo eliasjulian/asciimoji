212f1f0f00ad44b27ff24b43139c2d55155d1ee7
Elias Julian <eliasneverlost@gmail.com>
2019-04-21 15:59:37 -0500

    Navbar styling w/ logo + menu filter icon

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, FlatList, Dimensions } from "react-native";
+import styled from "styled-components";
+import SvgUri from "react-native-svg-uri";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Container>
+        <Navbar>
+          <SvgUri
+            width="180"
+            height="35"
+            source={require("./assets/logoblack.svg")}
+          />
+          <Menu source={require("./assets/menuicon.png")} />
+        </Navbar>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: white;
+`;
+
+const Navbar = styled.View`
+  width: 100%;
+  margin-top: 80px;
+  padding-left: 20px;
+  position: absolute;
+`;
+
+const Menu = styled.Image`
+  width: 35px;
+  height: 23px;
+  position: absolute;
+  right: 20;
+  top: 5;
+`;
